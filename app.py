5be3c36f539f904ede9e533b4e9a86f71383f415
valeria <silvvavaleria@gmail.com>
2020-04-27 14:38:13 -0300

    Streamlit app v2

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def grafico_scatterplot(x, y, df):
 
 #Correlacao
 def matriz_correlacao(numero):
-    st.markdown('**Correlação:**')
+    st.markdown('**Correlação**')
     st.write('A [correlação](https://medium.com/brdata/correla%C3%A7%C3%A3o-direto-ao-ponto-9ec1d48735fb) é uma análise descritiva que mede se há e qual o grau de dependência entre duas variáveis')
 
     st.markdown('**Correlação de Pearson:** Também chamado de “coeficiente de correlação produto-momento” ou simplesmente '
@@ -73,7 +73,6 @@ def matriz_correlacao(numero):
     sns.heatmap(corr, fmt='.2f', square=True, annot=True)
     st.pyplot()
 
-
 def main():
     st.title('Análise Exploratória de Dados')
     st.write('Esta página é dedicada ao desenvolvimento de uma aplicação de análise de dados '
@@ -83,14 +82,14 @@ def main():
     file = st.file_uploader('Faça upload da base de dados que deseja analisar (.csv)', type='csv')
 
     if file is not None:
-        df = pd.read_csv(file)
+        df = pd.read_csv(file, index_col=False)
         st.subheader('**Conhecendo sua base de dados**')
         st.write('O arquivo de sua base de dados possui ', df.shape[0],' linhas e ', df.shape[1],' colunas.')
 
         st.markdown('**Visualizando o Dataframe**')
         number = st.slider('Escolha o numero de linhas que deseja ver', min_value=2, max_value=10)
-        st.write('Você selecionou: ', number, 'linhas.')
         st.dataframe(df.head(number))
+        st.write('Você selecionou: ', number, 'linhas.')
 
         # Criando um DataFrame auxiliar(aux) para a análise dos dados
         #Esse dataframe retorna as colunas, os tipos de cada variável e a soma e porcentagem dos dados faltantes
@@ -101,7 +100,8 @@ def main():
                  'ausentes (nulos), assim como conhecer os tipos de dados analisados, é um dos primeiros passos '
                  'para uma análise consistente. ')
         st.write('Valores ausentes não tratados podem impactar na análise, podendo gerar conclusões equivocadas.')
-        st.write(aux)
+        ausentes= pd.DataFrame({'tipos': df.dtypes, 'Nulos (Total)': df.isna().sum(),'Nulos(%)': (df.isna().sum() / df.shape[0]) * 100})
+        st.write(ausentes)
 
         # Separando as variáveis numéricas e as variáveis categóricas
         st.markdown('**Variáveis numéricas: **')
@@ -129,55 +129,62 @@ def main():
             st.markdown('Selecione o que deseja analisar: ')
             mean = st.checkbox('Média')
             if mean:
-                st.markdown(df[col].mean())
+                st.markdown(df[col].mean().round(2))
                 st.write('A média é calculada somando-se todos os valores '
                          'de um conjunto de dados e dividindo-se pelo número de elementos deste conjunto.')
                 st.image('media.png', width=600)
             median = st.checkbox('Mediana')
             if median:
-                st.markdown(df[col].median())
+                st.markdown(df[col].median().round(2))
                 st.write('A mediana é o valor que separa um conjunto em duas quantidades iguais, ou seja,'
                          ' o valor central do conjunto.')
             desvio_pad = st.checkbox('Desvio Padrão')
             if desvio_pad:
-                st.markdown(df[col].std())
+                st.markdown(df[col].std().round(2))
                 st.write('O desvio padrão é uma medida que expressa o grau de dispersão de um conjunto de dados, '
                          'ou seja, indica um quanto um conjunto de dados é uniforme. Quanto mais próximo de zero'
                          'mais uniforme o conjunto de dados')
             kurtosis = st.checkbox('Kurtosis')
             if kurtosis:
-                st.markdown(df[col].kurtosis())
+                st.markdown(df[col].kurtosis().round(5))
                 st.write('A Curtose (kurtosis) é a medida de dispersão que caracteriza o “achamento” '
                          'da curva de função de distribuição. É medida em relação a curva Gaussiana '
                          '(normal).')
                 st.write('Uma curva leptocúrtica (>0) tem sua distribuiçãi mais afuniladam com pico '
                          'maior que o de uma distribuição Gaussiana e com caudas pesadas.')
-                st.write('Uma curva platicúrtica tem sua função de distribuição mais achatada que a '
+                st.write('Uma curva platicúrtica (<0) tem sua função de distribuição mais achatada que a '
                          'Gaussiana.')
                 st.image('kurtosis.jpg', width=600)
+
             skewness = st.checkbox('Skewness')
             if skewness:
-                st.markdown(df[col].skew())
+                st.markdown(df[col].skew().round(5))
                 st.write('A Skewness mede a falta de simetria nos dados. Diferencia os valores extremos em uma '
                          'cauda versus a outra. O valor de skewness em uma curva simétrica é zero.')
                 st.image('skewness.png', width=600)
             describe = st.checkbox('Describe')
             if describe:
                 st.table(df[col].describe().transpose())
-                st.write('')
-        st.subheader('Estatística Descritiva Bivariada/ Multivariada')
+                st.write('O Describe contém uma síntese das informações de Estatística Descritiva de uma determinadas '
+                         'coluna.')
+
+        st.subheader('**Estatística Descritiva Bivariada/ Multivariada**')
         st.write('A estatística descritiva bivariada/multivariada diz respeito ao relacionamento entre os '
                  'pares ou conjunto de variáveis como um todo.')
 
-        matriz_correlacao(df[colunas_numericas])
+        correlacao = st.checkbox('Correlação')
+        if correlacao:
+            matriz_correlacao(df[colunas_numericas])
 
         st.markdown('**Visualizações dos dados**')
 
         st.write('Aqui temos alguns exemplos mais comuns de visualização dos dados de forma gráfica. A melhor forma de'
                  'visualização depende muito dos tipos de dados analisados.')
 
-        histograma = st.checkbox('Histograma')
-        if histograma:
+        graficos =st.radio('Escolha a opção de visualização:',('Histograma','Gráfico de Barras', 'BoxPlot', 'ScatterPlot'))
+
+        if graficos == 'Histograma':
+            st.markdown('Histograma')
             col_num = st.selectbox('Selecione a Coluna Numérica', colunas_numericas[1:])
             st.markdown('Histograma da coluna: ' + str(col_num))
             st.write(grafico_histograma(col_num,df))
@@ -186,31 +193,30 @@ def main():
                      ' que torna os retângulos cada vez mais finos e altos), a distribuição de frequência torna–se uma '
                      'distribuição de densidade de probabilidades ')
 
-
-        barras = st.checkbox('Gráfico de Barras')
-        if barras:
+        if graficos == 'Gráfico de Barras':
+            st.markdown('Gráfico de Barras')
             col_num_barras = st.selectbox('Selecione a Coluna Numérica', colunas_numericas[1:])
-            col_cat_barras = st.selectbox('Selecione uma Coluna Categórica', colunas_object)
+            col_cat_barras = st.selectbox('Selecione uma Coluna Categórica', colunas_object[1:])
             st.write(grafico_barras(col_num_barras, col_cat_barras, df))
             st.write('O gráfico de barras  gráfico de barras é utilizado para realizar comparações entre as categorias '
                      'de uma variável qualitativa ou quantitativa discreta')
 
-        boxplot = st.checkbox('Boxplot')
-        if boxplot:
+        if graficos == 'BoxPlot':
+            st.markdown('BoxPlot')
             col_num_boxplot = st.selectbox('Selecione a Coluna Numérica', colunas_numericas[1:])
             col_cat_boxplot = st.selectbox('Selecione a uma Coluna Categórica', colunas_object[1:])
             st.write(grafico_boxplot(col_num_boxplot, col_cat_boxplot, df))
             st.write('Representa a variação de dados observados de uma variável numérica por meio de quartis. Os outliers'
                      'ficam como pontos individuais')
 
-
-        scatter = st.checkbox('ScatterPlot')
-        if scatter:
+        if graficos == 'ScatterPlot':
+            st.markdown('ScatterPlot')
             col_num_checkbox = st.selectbox('Selecione a Coluna Numérica', colunas_numericas[1:])
             col_cat_checkbox = st.selectbox('Selecione a uma Coluna Categórica', colunas_object[1:])
             st.write(grafico_scatterplot(col_num_checkbox, col_cat_checkbox, df))
             st.write('O gráfico de dispersão utiliza coordenadas cartesianas para exibir valores de um conjunto de dados.'
                      'Cada ponto é o valor de uma variável')
+
     st.markdown('**Valéria Cristina Silva**')
 
     st.sidebar.subheader('Sobre mim:')
